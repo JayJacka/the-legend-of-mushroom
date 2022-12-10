13eb91d4cf1bac2270c923e289c5786b18d3c0e9
Teerapat Chantaramanee <teerapatchantaramanee@gmail.com>
2022-12-10 19:34:27 +0700

    Add EnemyConstants & TomatoAnimations

--- src/utils/Animations.java
+++ src/utils/Animations.java
@@ -8,40 +8,87 @@ import sharedObject.RenderableHolder;
 public class Animations {
 	public static ArrayList<Image> getMushroomIdle() {
 		ArrayList x = new ArrayList<Image>();
-		x.add(RenderableHolder.mushRoomIdle0);x.add(RenderableHolder.mushRoomIdle1);
-		x.add(RenderableHolder.mushRoomIdle2);x.add(RenderableHolder.mushRoomIdle3);
-		x.add(RenderableHolder.mushRoomIdle4);x.add(RenderableHolder.mushRoomIdle5);
-		x.add(RenderableHolder.mushRoomIdle6);x.add(RenderableHolder.mushRoomIdle7);
-		//x.add(RenderableHolder.mushRoomIdle8);x.add(RenderableHolder.mushRoomIdle9);
+		x.add(RenderableHolder.mushRoomIdle0);
+		x.add(RenderableHolder.mushRoomIdle1);
+		x.add(RenderableHolder.mushRoomIdle2);
+		x.add(RenderableHolder.mushRoomIdle3);
+		x.add(RenderableHolder.mushRoomIdle4);
+		x.add(RenderableHolder.mushRoomIdle5);
+		x.add(RenderableHolder.mushRoomIdle6);
+		x.add(RenderableHolder.mushRoomIdle7);
 		return x;
 	}
 	
 	public static ArrayList<Image> getMushroomAttack(){
 		ArrayList x = new ArrayList<Image>();
-		x.add(RenderableHolder.mushRoomAttack0);x.add(RenderableHolder.mushRoomAttack1);
-		x.add(RenderableHolder.mushRoomAttack2);x.add(RenderableHolder.mushRoomAttack3);
-		x.add(RenderableHolder.mushRoomAttack4);x.add(RenderableHolder.mushRoomAttack5);
-		x.add(RenderableHolder.mushRoomAttack6);x.add(RenderableHolder.mushRoomAttack7);
+		x.add(RenderableHolder.mushRoomAttack0);
+		x.add(RenderableHolder.mushRoomAttack1);
+		x.add(RenderableHolder.mushRoomAttack2);
+		x.add(RenderableHolder.mushRoomAttack3);
+		x.add(RenderableHolder.mushRoomAttack4);
+		x.add(RenderableHolder.mushRoomAttack5);
+		x.add(RenderableHolder.mushRoomAttack6);
+		x.add(RenderableHolder.mushRoomAttack7);
 		return x;
 	}
 	
 	public static ArrayList<Image> getMushroomJump(){
 		ArrayList x = new ArrayList<Image>();
-		x.add(RenderableHolder.mushRoomJump0);x.add(RenderableHolder.mushRoomJump1);
-		x.add(RenderableHolder.mushRoomJump2);x.add(RenderableHolder.mushRoomJump3);
-		x.add(RenderableHolder.mushRoomJump4);x.add(RenderableHolder.mushRoomJump5);
-		x.add(RenderableHolder.mushRoomJump6);x.add(RenderableHolder.mushRoomJump7);
-		//x.add(RenderableHolder.mushRoomJump8);
+		x.add(RenderableHolder.mushRoomJump0);
+		x.add(RenderableHolder.mushRoomJump1);
+		x.add(RenderableHolder.mushRoomJump2);
+		x.add(RenderableHolder.mushRoomJump3);
+		x.add(RenderableHolder.mushRoomJump4);
+		x.add(RenderableHolder.mushRoomJump5);
+		x.add(RenderableHolder.mushRoomJump6);
+		x.add(RenderableHolder.mushRoomJump7);
 		return x;
 	}
 	
 	public static ArrayList<Image> getMushroomWalk(){
 		ArrayList x = new ArrayList<Image>();
-		x.add(RenderableHolder.mushRoomWalk0);x.add(RenderableHolder.mushRoomWalk1);
-		x.add(RenderableHolder.mushRoomWalk2);x.add(RenderableHolder.mushRoomWalk3);
-		x.add(RenderableHolder.mushRoomWalk4);x.add(RenderableHolder.mushRoomWalk5);
-		x.add(RenderableHolder.mushRoomWalk6);x.add(RenderableHolder.mushRoomWalk7);
-		//x.add(RenderableHolder.mushRoomWalk8);x.add(RenderableHolder.mushRoomWalk9);
+		x.add(RenderableHolder.mushRoomWalk0);
+		x.add(RenderableHolder.mushRoomWalk1);
+		x.add(RenderableHolder.mushRoomWalk2);
+		x.add(RenderableHolder.mushRoomWalk3);
+		x.add(RenderableHolder.mushRoomWalk4);
+		x.add(RenderableHolder.mushRoomWalk5);
+		x.add(RenderableHolder.mushRoomWalk6);
+		x.add(RenderableHolder.mushRoomWalk7);
 		return x;
 	}
+	
+	public static ArrayList<Image> getTomatoIdle(){
+		ArrayList x = new ArrayList<Image>();
+		x.add(RenderableHolder.TomatoIdle0);
+		x.add(RenderableHolder.TomatoIdle1);
+		x.add(RenderableHolder.TomatoIdle2);
+		x.add(RenderableHolder.TomatoIdle3);
+		x.add(RenderableHolder.TomatoIdle4);
+		x.add(RenderableHolder.TomatoIdle5);
+		return x;
+	}
+	
+	public static ArrayList<Image> getTomatoWalk(){
+		ArrayList x = new ArrayList<Image>();
+		x.add(RenderableHolder.TomatoIdle0);
+		x.add(RenderableHolder.TomatoIdle1);
+		x.add(RenderableHolder.TomatoIdle2);
+		x.add(RenderableHolder.TomatoIdle3);
+		x.add(RenderableHolder.TomatoIdle4);
+		x.add(RenderableHolder.TomatoIdle5);
+		return x;
+	}
+	
+	public static ArrayList<Image> getTomatoHit(){
+		ArrayList x = new ArrayList<Image>();
+		x.add(RenderableHolder.TomatoHit0);
+		x.add(RenderableHolder.TomatoHit1);
+		x.add(RenderableHolder.TomatoHit2);
+		x.add(RenderableHolder.TomatoHit3);
+		x.add(RenderableHolder.TomatoHit4);
+		x.add(RenderableHolder.TomatoHit5);
+		return x;
+	}
+	
 }
--- src/utils/Constants.java
+++ src/utils/Constants.java
@@ -24,4 +24,24 @@ public class Constants {
 		public static final int UP = 2;
 		public static final int DOWN = 3;
 	}
+	
+	public static class EnemyConstants{
+		public static final int TOMATO = 0;
+		
+		public static final int IDLE = 0;
+		public static final int ATTACK = 1;
+		public static final int WALK = 2;
+		
+		public static int GetSpriteAmount(int Type, int State) {
+			switch(Type) {
+				case TOMATO:
+					switch(Type) {
+					case IDLE : return 6;
+					case WALK : return 6;
+					case ATTACK : return 6;
+					}
+			}
+			return 0;
+		}
+	}
 }
