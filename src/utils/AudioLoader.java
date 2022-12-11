d796121baed62de68e24b6a0667b408cbc328c18
Teerapat Chantaramanee <teerapatchantaramanee@gmail.com>
2022-12-11 10:53:03 +0700

    Change folder AudioLoader

--- src/utils/AudioLoader.java
+++
@@ -1,7 +0,0 @@
-package utils;
-
-import javafx.scene.media.AudioClip;
-
-public class AudioLoader {
-	public static AudioClip PlayerAttack = new AudioClip(ClassLoader.getSystemResource("/sounds/PlayerAttack.wav").toString());
-}
